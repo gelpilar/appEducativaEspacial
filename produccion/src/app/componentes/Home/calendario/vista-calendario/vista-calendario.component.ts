ead8ab8e58126cc12c854d176ad6486e0eefd5b1
gelpilar <pilarscollo@gmail.com>
2023-11-09 12:16:31 -0300

    arreglos esteticos

--- produccion/src/app/componentes/Home/calendario/vista-calendario/vista-calendario.component.ts
+++ produccion/src/app/componentes/Home/calendario/vista-calendario/vista-calendario.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { ChangeDetectorRef, Component } from '@angular/core';
+import { FlexAlignDirective } from '@angular/flex-layout';
 
 @Component({
   selector: 'app-vista-calendario',
@@ -6,5 +7,25 @@ import { Component } from '@angular/core';
   styleUrls: ['./vista-calendario.component.css']
 })
 export class VistaCalendarioComponent {
+  flag:boolean=false;
+ 
+  constructor(private cd: ChangeDetectorRef)
+  {
+    this.flag=false;
+  }
+
+
+
+ estado() {
+    if(this.flag)
+    {
+      this.flag=false;
+      
+    }else
+    {
+      this.flag=true;
+    }
+    this.cd.detectChanges();
+  }
 
 }
